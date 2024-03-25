c7af3eccd97f7f5ff453f7e24bf5ef56265b07da
Pate <zapate@deloitte.com>
2024-03-25 09:11:10 -0400

    Updates to install packages file

--- Year 2/Install_packages.R
+++ Year 2/Install_packages.R
@@ -7,11 +7,29 @@
 # History of Modification:
 #         N/A
 
-install.packages("car")
-install.packages("caret")
-install.packages("tidyr")
-install.packages("glmnet") 
-install.packages("corrplot")
-install.packages("ggplot2")
-install.packages("tidyverse")
-install.packages("omnibus") 
+#R version = 4.2.2
+
+#User needs 'Devtools' package in order to install specific version of packages
+install.packages("devtools")
+require(devtools)
+
+install.version('car', '3.1.2', repos = "http://cran.us.r-project.org")
+install.version('caret', '6.0.94', repos = "http://cran.us.r-project.org")
+install.version('tidyr', '1.3.1', repos = "http://cran.us.r-project.org")
+install.version('glmnet', '4.1.8', repos = "http://cran.us.r-project.org")
+install.version('corrplot', '0.92', repos = "http://cran.us.r-project.org")
+install.version('ggplot2', '3.4.4', repos = "http://cran.us.r-project.org")
+install.version('tidyverse', '2.0.0', repos = "http://cran.us.r-project.org")
+install.version('omnibus', '1.2.9', repos = "http://cran.us.r-project.org")
+
+
+library(car)
+library(caret)
+library(tidyr)
+library(glmnet) #documentation at: https://glmnet.stanford.edu/articles/glmnet.html
+library(corrplot)
+library(ggplot2)
+library(tidyverse)
+library(omnibus) 
+
+
